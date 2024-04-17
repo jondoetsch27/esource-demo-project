2782ea3db7aaac795147a3899c1c282d74b05319
Jonathon Doetsch <jonathon.doetsch@gmail.com>
2024-04-17 14:23:25 -0700

    forgot to use min/max from @Configuration in RandomNumberService

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # esource-demo-project
 ```
+# Run mvn clean:install to build .jar #
+
 docker build -t esource/demo .
 docker run -p 8080:8080 esource/demo
 curl -o output.json http://localhost:8080/run
 
-## Linux Terminal
+## for Linux Terminal ##
 cat output.json
 
-## Windows PowerShell
+## for Windows PowerShell ##
 Get-Content .\output.json
 ```
--- src/main/java/com/jdd/esource_demo_project/service/RandomNumberService.java
+++ src/main/java/com/jdd/esource_demo_project/service/RandomNumberService.java
@@ -1,19 +1,25 @@
 package com.jdd.esource_demo_project.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
 public class RandomNumberService {
-    private static final int MIN = 0;
-    private static final int MAX = 50;
+
+    private final Integer getMin;
+    private final Integer getMax;
+    public RandomNumberService(@Qualifier("getMin") Integer getMin, @Qualifier("getMax") Integer getMax) {
+        this.getMin = getMin;
+        this.getMax = getMax;
+    }
 
     public Integer[][] generateRandomNumbers() {
         Integer[][] numbers = new Integer[3][15];
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
-                numbers[i][j] = new Random().nextInt(MIN, MAX);
+                numbers[i][j] = new Random().nextInt(getMin, getMax);
             }
         }
         return numbers;
